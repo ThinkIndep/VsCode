f81a0b11374a706f68e4bdaa5dc6103ea6c47c77
ThinkIndep <issacsu@outlook.com>
2017-04-17 21:00:55 +0800

    初步完成不同角色页面

---
+++ WebSite2/StuLogin.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+
+public partial class StuLogin : System.Web.UI.Page
+{
+    SqlConnection sqlcon;
+    SqlCommand sqlcom;
+    string strCon = "Server=(localdb)\\Projects;Initial Catalog=StuManager;Integrated Security=true";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            bind();
+        }
+    }
+    protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        GridView1.EditIndex = e.NewEditIndex;
+        bind();
+    }
+
+    //删除
+    protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        string sqlstr = "delete from dbo.StuInfo where StuID='" + GridView1.DataKeys[e.RowIndex].Value.ToString() + "'";
+        sqlcon = new SqlConnection(strCon);
+        sqlcom = new SqlCommand(sqlstr, sqlcon);
+        sqlcon.Open();
+        sqlcom.ExecuteNonQuery();
+        sqlcon.Close();
+        bind();
+    }
+
+    //更新
+    protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        sqlcon = new SqlConnection(strCon);
+        string sqlstr = "update dbo.StuInfo set StuName=N'"
+        + ((TextBox)(GridView1.Rows[e.RowIndex].Cells[1].Controls[0])).Text.ToString().Trim() + "',StuGender=N'"
+        + ((TextBox)(GridView1.Rows[e.RowIndex].Cells[2].Controls[0])).Text.ToString().Trim() + "',StuNation=N'"
+        + ((TextBox)(GridView1.Rows[e.RowIndex].Cells[3].Controls[0])).Text.ToString().Trim() + "',StuCollege=N'"
+        + ((TextBox)(GridView1.Rows[e.RowIndex].Cells[4].Controls[0])).Text.ToString().Trim() + "',StuMajor=N'"
+        + ((TextBox)(GridView1.Rows[e.RowIndex].Cells[5].Controls[0])).Text.ToString().Trim() + "',StuPassword=N'"
+        + ((TextBox)(GridView1.Rows[e.RowIndex].Cells[6].Controls[0])).Text.ToString().Trim() + "',StuInterest=N'"
+        + ((TextBox)(GridView1.Rows[e.RowIndex].Cells[7].Controls[0])).Text.ToString().Trim() + "',StuPhone=N'"
+        + ((TextBox)(GridView1.Rows[e.RowIndex].Cells[8].Controls[0])).Text.ToString().Trim() + "',StuEmail=N'"
+        + ((TextBox)(GridView1.Rows[e.RowIndex].Cells[9].Controls[0])).Text.ToString().Trim() + "' where StuID='"
+        + GridView1.DataKeys[e.RowIndex].Value.ToString() + "'";
+        sqlcom = new SqlCommand(sqlstr, sqlcon);
+        sqlcon.Open();
+        sqlcom.ExecuteNonQuery();
+        sqlcon.Close();
+        GridView1.EditIndex = -1;
+        bind();
+    }
+
+    //取消
+    protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        GridView1.EditIndex = -1;
+        bind();
+    }
+    public void bind()
+    {
+        string sqlstr = "select * from dbo.StuInfo";
+        sqlcon = new SqlConnection(strCon);
+        SqlDataAdapter myda = new SqlDataAdapter(sqlstr, sqlcon);
+        DataSet myds = new DataSet();
+        sqlcon.Open();
+        myda.Fill(myds, "dbo.StuInfo");
+        GridView1.DataSource = myds;
+        GridView1.DataKeyNames = new string[] { "StuID" };//主键
+        GridView1.DataBind();
+        sqlcon.Close();
+    }
+}
